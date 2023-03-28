bb565803c5afd656cc0164a5c686e3b2add9df9e
LuisAceves <comps@colostate.edu>
2023-03-28 16:25:06 -0600

    hello

--- fuel/app/views/MTNL/one.php
+++ fuel/app/views/MTNL/one.php
@@ -9,5 +9,11 @@
             My name is Marlowe Lankford and I am a Sophomore. I am studying computer science and plan to graduate Spring 2025.  
             </p>
         </div>
+        <div>
+            <img height = 100 width = 100 src = "https://csulife.source.colostate.edu/wp-content/uploads/sites/10/2021/10/CAM-the-ram.jpg">
+            <p>
+            My name is Luis Aceves and I am a Junior. I am studying computer science and hope to be a penetration tester!
+            </p>
+        </div>
     </section>
 </main>
