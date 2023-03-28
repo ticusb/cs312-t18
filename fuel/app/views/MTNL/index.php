f0db87daf532b3863f11b8f8367cdcc79b090f38
moelankford4 <moelankf@colostate.edu>
2023-03-28 15:55:19 -0600

    p

--- fuel/app/views/MTNL/index.php
+++ fuel/app/views/MTNL/index.php
@@ -3,11 +3,7 @@
         <h1>MTNL</h1>
         <?php echo Asset::img('MTNLlogo.png'); ?>
     </section>
-    <section class="body-section">
-        <p>Hey! My name is Ticus, welcome to my page! Extremely excited to have you here, truly. In the following paragraphs I will discuss east and west related things.</p>
-        <p>My favorite east state has got to be New York. I've always wanted to live in a big city, I feel like I would thrive. All of the other east coast states are mid, prove me wrong. If I were to go to the east hemisphere I would ski in Europe, or see Dubai!!!</p>
-        <p>I also have a mate in Austrailia, he would flip if I visited him. Being from Alaska we are the furthest from eachother. I can't really think of more east things to say, but I'll add them when I do. This page was made by the East Crew.</p>
-        <p>My favorite west state has got to be Alaska duhhh, unless you haven't seen the east page, then not duhh. But yes Alaska is my home I have to rep. If you have never gone I highly recommend, it is the most beautiful state in all the world. Feel free to name drop me. (It wont do much, my business professor said that one time so now I say it)</p>
-        <p>I'll probably stay west coast most of my life, unless I stumble on money. I feel like we have the best food chains, In & Out, Chic-Fil-a, Fatburger, etc. This post was created by West Crew.</p>
-    </section>
+    <div>
+        
+    </div>
 </main>
