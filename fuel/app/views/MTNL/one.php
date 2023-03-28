0fb72ce87729cae28c5c83e1c68329fae3b6d783
nrkorte <71001714+nrkorte@users.noreply.github.com>
2023-03-28 15:30:59 -0600

    my bio

--- fuel/app/views/MTNL/one.php
+++ fuel/app/views/MTNL/one.php
@@ -8,6 +8,10 @@
             <p>
             My name is Marlowe Lankford and I am a Sophomore. I am studying computer science and plan to graduate Spring 2025.  
             </p>
+            <img height="100" width="100" src="https://brand.colostate.edu/wp-content/uploads/sites/47/2019/01/Ram-Logo-Pantone-357-617-768x768.jpg">
+            <p>
+                My name is Nick Korte and I am a Junior studying computer science at Colorado State (Go Rams!) and I am excited to learn more about web development!
+            </p>
         </div>
     </section>
 </main>
