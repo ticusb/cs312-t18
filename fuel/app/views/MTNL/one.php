a51df8e6c490328b46966012778e10837e14b7a3
moelankford4 <moelankf@colostate.edu>
2023-03-28 14:53:58 -0600

    m

--- fuel/app/views/MTNL/one.php
+++ fuel/app/views/MTNL/one.php
@@ -6,7 +6,7 @@
         <div>
             <img height = 100 width = 100 src = "https://brand.colostate.edu/wp-content/uploads/sites/47/2019/01/Ram-Logo-Pantone-357-617-768x768.jpg">
             <p>
-            My name is Marlowe Lankford and I am a Sophomore. 
+            My name is Marlowe Lankford and I am a Sophomore. I am studying computer science and plan to graduate Spring 2025.  
             </p>
         </div>
     </section>
