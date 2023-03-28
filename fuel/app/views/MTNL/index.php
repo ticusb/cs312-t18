5341e45d16da12d52fc920846bbe1c9208d09d56
moelankford4 <moelankf@colostate.edu>
2023-03-28 16:00:25 -0600

    kj

--- fuel/app/views/MTNL/index.php
+++ fuel/app/views/MTNL/index.php
@@ -4,6 +4,9 @@
         <?php echo Asset::img('MTNLlogo.png'); ?>
     </section>
     <div>
-        
+        <p>
+            We want to make the world a better place.
+            Carbon Neutral, Go Green.
+        </p>
     </div>
 </main>
