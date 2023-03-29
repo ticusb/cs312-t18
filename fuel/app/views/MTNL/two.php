400ab35e0286d47c78b7acc1b343fdc42f666b81
ticusb <LeviticusB02@gmail.com>
2023-03-29 13:50:28 -0600

    Fixed table

--- fuel/app/views/MTNL/two.php
+++ fuel/app/views/MTNL/two.php
@@ -1,11 +1,11 @@
 
-<main id="all">
-    <section class="cc">
-        <div id="overlay"></div>
-        <div id="printpage"></div>
+<main id = "all">
+    <section class="cc" id = "interactable">
         <h1>Color Coordinate Generation</h1>
-        <table id="firstTbl">
+        <table id="firstTbl"></table>
             <script type="text/javascript">
+                const body = document.getElementsByClassName("cc");
+
                 const queryString = window.location.search;
                 const urlParams = new URLSearchParams(queryString);
                 let rows = urlParams.get('rows/cols');
@@ -19,8 +19,28 @@
                     }
                 }
 
+                
+                tbl2 = document.createElement('table');
+                tbl2.style.border = '1px solid black';
+                for (var i = 0; i < rc; i++) {
+                    const tr = tbl2.insertRow();
+                    for (var j = 0; j < rc; j++) {
+                        if (i == 0 && j == 0) {
+                            const td = tr.appendChild(document.createTextNode(""));
+                        } else if (i == 0) {
+                            const td = tr.appendChild(document.createTextNode(String.fromCharCode('A'.charCodeAt() + (i - 1))));
+                        } else if (i != 0 && j == 0) {
+                            const td = tr.appendChild(document.createTextNode("" + i));
+                        } else {
+                            const td = tr.appendChild(document.createTextNode(""));
+                        }
+                        
+                    }
+                }
 
-                function set_style(id, attr, value){
+                body.appendChild(tbl2);
+                
+                function set_style(id, attr, value) {
                     var item = document.getElementById(id);
                     item.style[attr] = value;
                 }
@@ -29,22 +49,13 @@
                 }
 
                 function generate_view() {
-                    // window.print();
-                    set_style("overlay", "z-index", "100");
-                    set_style("overlay", "opacity", "0.8");
-                    set_style("printpage", "z-index", "101");
-                    
-                }
-                function generate_view() {
                     document.getElementById("all").style.filter = "grayscale(100%)";
+                    document.getElementById("all").disabled = true;
                 }
             </script>
 
-
-
-
-        </table>
-        <button class="button_one" onclick="generate_view();"> Print </button>
-
+            <button class="button_one" onclick="generate_view();">
+                Print
+            </button>
     </section>
 </main>
